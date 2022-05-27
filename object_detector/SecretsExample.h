0b7fb7f9a845e55cc5abd074deb418268719e1a3
Krystian Gaczyński <30621967+krygacz@users.noreply.github.com>
2022-05-27 03:05:25 +0200

    Add secrets example

---
+++ object_detector/SecretsExample.h
@@ -0,0 +1,4 @@
+#define WIFI_SSID "your wifi ssid"
+#define WIFI_PASS "your wifi password"
+#define HA_URL "your home assistant url (without 'https://')"
+#define HA_TOKEN "your home assistant access token"
